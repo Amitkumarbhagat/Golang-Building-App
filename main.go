cf2c6d59691bf72b7150999c726eecf45708562b
Amit Bhagat <amitbhagat2506@gmail.com>
2023-01-20 10:22:04 +0530

    error handling eg divide by zero messages etc

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -24,10 +25,29 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(writer http.ResponseWriter, request *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(writer, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(writer, fmt.Sprintf("%f divide by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 //main is application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
