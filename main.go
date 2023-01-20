bfd0265ab6a662c42bf323f267462f4fae03a7a7
Amit Bhagat <amitbhagat2506@gmail.com>
2023-01-20 10:01:10 +0530

    created home, about and root page of web application

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ const portNumber = ":8080"
 
 //Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the home page")
+	_, _ = fmt.Fprintf(w, "this is the home page")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
-	fmt.Fprintf(w, fmt.Sprintf("this is the about page and 2 + 2 is %d ", sum))
-	fmt.Fprintf(w, "this is the about page")
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("this is the about page and 2 + 2 is %d ", sum))
+	_, _ = fmt.Fprintf(w, "this is the about page")
 }
 
 //addValues adds two integers and return the sum
