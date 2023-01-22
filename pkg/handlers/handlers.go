1f8ae7306095cba3afa9f77679b193a54a593c52
Amit Bhagat <amitbhagat2506@gmail.com>
2023-01-22 23:31:22 +0530

    setting application wide configuration

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,61 @@
+//package handlers
+//
+//import (
+//	"github.com/amitkumarbhagat/golang/pkg/render"
+//	"net/http"
+//)
+//
+////Home is the home page handler
+//func Home(w http.ResponseWriter, r *http.Request) {
+//	render.RenderTemplate(w, "home.page.tmpl")
+//}
+//
+//// About is the about page handler
+//func About(w http.ResponseWriter, r *http.Request) {
+//	render.RenderTemplate(w, "about.page.tmpl")
+//}
+
 package handlers
 
 import (
+	"github.com/amitkumarbhagat/golang/pkg/config"
 	"github.com/amitkumarbhagat/golang/pkg/render"
 	"net/http"
 )
 
-//Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
 }
 
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
