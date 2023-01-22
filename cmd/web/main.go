819bca78ab6185457015f7b87393b003b6db8ab1
Amit Bhagat <amitbhagat2506@gmail.com>
2023-01-22 23:21:38 +0530

    setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,3 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"github.com/amitkumarbhagat/golang/pkg/config"
-//	"github.com/amitkumarbhagat/golang/pkg/handlers"
-//	"github.com/amitkumarbhagat/golang/pkg/render"
-//	"net/http"
-//)
-//
-//const portNumber = ":8080"
-//
-////main is application function
-//func main() {
-//	var app config.AppConfig
-//
-//	tc, err := render.CreateTemplateCache()
-//	http.HandleFunc("/", handlers.Home)
-//	http.HandleFunc("/about", handlers.About)
-//
-//	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
-//	_ = http.ListenAndServe(portNumber, nil)
-//}
-//
-//
-
 package main
 
 import (
